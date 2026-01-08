93886e46203026ba33a7d603166287247975766a
Thien Tu <tt.ngo@student.maastrichtuniversity.nl>
2026-01-08 22:38:12 +0100

    Pull data from supabase and create jupyter notebook for plotting

---
+++ Python-Scripts/Analysis/PullData.py
@@ -0,0 +1,29 @@
+from supabase import create_client
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+import pandas as pd
+
+
+
+
+current_script_dir = Path(__file__).parent.parent
+env_path = current_script_dir / ".env"
+load_dotenv(env_path)
+
+
+def pull_data_from_supabase() -> pd.DataFrame:
+    training_table = os.environ.get("TRAINING_TABLE")
+    url = os.environ.get("SUPABASE_URL")
+    key = os.environ.get("SUPABASE_KEY")
+
+    supabase = create_client(url,key)
+
+    response = supabase.table(training_table) \
+    .select("*") \
+    .eq("error_occurred", False) \
+    .execute()
+    data = response.data
+
+    return pd.DataFrame(data)
+
